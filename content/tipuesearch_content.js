d365e87cf63bb1de09f330c0e29c322660547957
yutang327 <tw0932582371@gmail.com>
2023-03-06 11:10:40 +0800

    add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '123', 'tags': '', 'url': 'About.html'}]};
